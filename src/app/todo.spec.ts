65ec4a6d10b73279ee435d836a57f9a483263da0
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-03-19 18:50:26 +0100

    renew

---
+++ src/app/todo.spec.ts
@@ -0,0 +1,7 @@
+import { TODO } from './todo';
+
+describe('TODO', () => {
+  it('should create an instance', () => {
+    expect(new TODO()).toBeTruthy();
+  });
+});
---
+++ src/app/todo.ts
@@ -0,0 +1,5 @@
+export class TODO {
+  id: string;
+  title: string;
+  done: boolean;
+}
